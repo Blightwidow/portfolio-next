dcfbe6ecf7967764a97dd715d55c57cf89cb9956
Theo Dammaretz <theo@dammaretz.fr>
2023-08-23 11:20:01 +0200

    chore: add opendata to work

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 
 import webdev from "@/media/web-dev.jpg"
 import ai from "@/media/ai.jpg"
+import data from "@/media/data.jpg"
 import { ImageCard } from "@/ui/shared/ImageCard"
 
 export default function Home() {
@@ -13,7 +14,6 @@ export default function Home() {
       </Head>
       <div className="row">
         <div className="four columns">
-          <div>
             <ImageCard
               image={webdev}
               title="Web Development"
@@ -22,13 +22,22 @@ export default function Home() {
               alt="a photo of a computer"
               sizes="(max-width: 549px) 100vw, 33vw"
             />
-          </div>
         </div>
         <div className="four columns">
           <ImageCard
             image={ai}
             title="Artificial Intelligence"
             href="/work/ai"
+            width={341}
+            alt="a 3d render of an android robot"
+            sizes="(max-width: 549px) 100vw, 33vw"
+          />
+        </div>
+        <div className="four columns">
+          <ImageCard
+            image={data}
+            title="Open Data"
+            href="https://data.dammaretz.fr"
             width={341}
             alt="a 3d render of an android robot"
             sizes="(max-width: 549px) 100vw, 33vw"
